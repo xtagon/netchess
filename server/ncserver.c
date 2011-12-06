6dabdd559a29427a5ec1d59890593b5d2c5e455b
Riju M <spikey360@yahoo.co.in>
2011-12-06 12:51:58 +0530

    Written startServer()

--- server/ncserver.c
+++ server/ncserver.c
@@ -1,16 +1,17 @@
 #include "linsock.h"
-#include "../board.h"
+#include "ncserver.h"
 
 int state;
 int sock;
 int conn;
+int turn;
 size_t len;
 struct sockaddr_in servaddr;
-struct sockaddr_in cliadddr;
+struct sockaddr_in cliaddr;
 
 int startServer(){
 state=STATE_CONNECTING;
-bzero(servaddr,sizeof(servaddr));
+bzero(&servaddr,sizeof(servaddr));
 servaddr.sin_family=AF_INET;
 servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
 servaddr.sin_port=htons(CHESSPORT);
@@ -20,12 +21,12 @@ fprintf(stderr,"Error creating socket\n");
 return ERROR;
 }
 //bind
-if(bind(sock,(struct sockaddr*)servaddr,sizeof(servaddr))<0){
-fprint(stderr,"Bind error\n");
+if(bind(sock,(struct sockaddr*)&servaddr,sizeof(servaddr))<0){
+fprintf(stderr,"Bind error\n");
 return ERROR;
 }
 //listen
-if(bind(sock,LISTENQ)<0){
+if(listen(sock,QUEUESIZE)<0){
 fprintf(stderr,"Listening error\n");
 return ERROR;
 }
@@ -36,8 +37,14 @@ fprintf(stderr,"Connection error\n");
 return ERROR;
 };
 state=STATE_CONNECTED;
+turn=TEAM_WHITE;
 return OK;
 }
-int isMatchSet(); //0 if set -1 otherwise
-int movePiece(coordinate_t src, coordinate_t dest);
+int isMatchSet(){}
+int movePiece(coordinate_t src, coordinate_t dest){
+if(state==STATE_PLAYING && turn==TEAM_WHITE){
+
+}
+return ERROR;
+}
 
--- server/ncserver.h
+++ server/ncserver.h
@@ -1,12 +1,16 @@
 #ifndef NCSERVER_H
 #define NCSERVER_H
+
 #include "../board.h"
+
 #define CHESSPORT 21000
 #define OK 0
 #define ERROR -1
+
 #define STATE_CONNECTING  0x010
 #define STATE_CONNECTED 0x020
 #define STATE_READY 0x030
+#define STATE_PLAYING 0x040
 
 int startServer(); //start the chess server
 int isMatchSet(); //0 if set -1 otherwise
